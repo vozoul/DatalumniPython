15fff1e317a1f309e40ca0bc29d1873b9109ac6c
vozoul <vozoul@gmail.com>
2023-05-04 10:56:29 +0200

    Datalumni - Python3 test

--- students.py
+++ students.py
@@ -47,31 +47,3 @@ def main():
 
 
 main()
-
-# if __name__ == "__main__":
-
-    # filename = 'students.csv'
-
-    # Ecrivez une fonction permettant de récupérer toutes les lignes
-    # du fichier CSV dans une list() `rows`
-    # rows = #
-    # print(f'\nLe fichier brut contient {len(rows)} lignes')
-
-    # Les étudiants ont chacun un diplôme qui leur est attribué
-    # La variable `degrees` contient la liste des diplômes
-    # degrees = #
-    # print(f'\nLe fichier contient {len(degrees)} diplômes uniques')
-
-    # Donnez, dans un dict, pour chaque diplôme le nombre d'étudiant
-    # par catégorie d'utilisateur (student, alumni, ...)
-    #
-    #   - Master Un -> Student : 123
-    #               -> Alumni : 456
-    #               -> ...
-    #   - Master Deux -> ...
-    #
-    # users_per_degree = # TODO
-
-    # Enregistrez le dictionnaire dans un nouveau fichier `degree_count.json`
-    # TODO
-    # print(f'\nFichier `degree_count.json` enregistré !')
